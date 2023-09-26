a2797b7b124ed4acb221da76608785f3b9434a60
jbagger <jbagger@student.hive.fi>
2023-09-26 14:39:32 +0300

    Added check to see if map is surrounded by walls

--- src/flooding_algorithm.c
+++ src/flooding_algorithm.c
@@ -40,6 +40,35 @@ int all_walls_changed(char **map)
 
 //------------------------------------------------------------------------------
 
+int surrounded_by_walls(char **map)
+{
+	int row;
+	int column;
+
+	row = -1;
+	while (map[++row])
+	{
+		column = -1;
+		while (map[row][++column])
+		{
+			if (map[row][column] == '0')
+			{
+				if (row == 0 || !map[row + 1])
+					return (FALSE);
+				if (column == 0 || map[row][column + 1] == '\0')
+					return (FALSE);
+				if (map[row - 1][column] == ' ' || map[row + 1][column] == ' ')
+					return (FALSE);
+				if (map[row][column - 1] == ' ' || map[row][column + 1] == ' ')
+					return (FALSE);
+			}
+		}
+	}
+	return (TRUE);
+}
+
+//------------------------------------------------------------------------------
+
 int check_map_validity(char **map)
 {
 	int row;
@@ -53,6 +82,8 @@ int check_map_validity(char **map)
 	flood(map, row, column);
 	print_map(map); // DEBUG
 	if (!all_walls_changed(map))
+		return (err("Map is not connected"));
+	if (!surrounded_by_walls(map))
 		return (err("Map is not surrounded by walls"));
 	return (SUCCESS);
 }

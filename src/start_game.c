6f46249d1744ca113b058cc8f5f85ab740bd284d
jbagger <jbagger@student.hive.fi>
2023-10-02 17:50:42 +0300

    Removed clunkyness by moving update() into a mlx_loop_hook()

--- src/start_game.c
+++ src/start_game.c
@@ -1,30 +1,34 @@
 #include "../incl/cub3d.h"
 
-void get_input(mlx_key_data_t *keydata, cub3d_t *cub3d)
+void get_input(mlx_key_data_t keydata, void *param)
 {
-	if (keydata->key == MLX_KEY_W && keydata->action == MLX_PRESS)
+	cub3d_t *cub3d;
+
+	cub3d = param;
+	handle_escape_key(&keydata, cub3d->mlx);
+	if (keydata.key == MLX_KEY_W && keydata.action == MLX_PRESS)
 		cub3d->keys.w = TRUE;
-	else if (keydata->key == MLX_KEY_A && keydata->action == MLX_PRESS)
+	else if (keydata.key == MLX_KEY_A && keydata.action == MLX_PRESS)
 		cub3d->keys.a = TRUE;
-	else if (keydata->key == MLX_KEY_S && keydata->action == MLX_PRESS)
+	else if (keydata.key == MLX_KEY_S && keydata.action == MLX_PRESS)
 		cub3d->keys.s = TRUE;
-	else if (keydata->key == MLX_KEY_D && keydata->action == MLX_PRESS)
+	else if (keydata.key == MLX_KEY_D && keydata.action == MLX_PRESS)
 		cub3d->keys.d = TRUE;
-	else if (keydata->key == MLX_KEY_LEFT && keydata->action == MLX_PRESS)
+	else if (keydata.key == MLX_KEY_LEFT && keydata.action == MLX_PRESS)
 		cub3d->keys.left = TRUE;
-	else if (keydata->key == MLX_KEY_RIGHT && keydata->action == MLX_PRESS)
+	else if (keydata.key == MLX_KEY_RIGHT && keydata.action == MLX_PRESS)
 		cub3d->keys.right = TRUE;
-	else if (keydata->key == MLX_KEY_W && keydata->action == MLX_RELEASE)
+	else if (keydata.key == MLX_KEY_W && keydata.action == MLX_RELEASE)
 		cub3d->keys.w = FALSE;
-	else if (keydata->key == MLX_KEY_A && keydata->action == MLX_RELEASE)
+	else if (keydata.key == MLX_KEY_A && keydata.action == MLX_RELEASE)
 		cub3d->keys.a = FALSE;
-	else if (keydata->key == MLX_KEY_S && keydata->action == MLX_RELEASE)
+	else if (keydata.key == MLX_KEY_S && keydata.action == MLX_RELEASE)
 		cub3d->keys.s = FALSE;
-	else if (keydata->key == MLX_KEY_D && keydata->action == MLX_RELEASE)
+	else if (keydata.key == MLX_KEY_D && keydata.action == MLX_RELEASE)
 		cub3d->keys.d = FALSE;
-	else if (keydata->key == MLX_KEY_LEFT && keydata->action == MLX_RELEASE)
+	else if (keydata.key == MLX_KEY_LEFT && keydata.action == MLX_RELEASE)
 		cub3d->keys.left = FALSE;
-	else if (keydata->key == MLX_KEY_RIGHT && keydata->action == MLX_RELEASE)
+	else if (keydata.key == MLX_KEY_RIGHT && keydata.action == MLX_RELEASE)
 		cub3d->keys.right = FALSE;
 }
 
@@ -47,13 +51,11 @@ void draw_background(cub3d_t *cub3d)
 	}
 }
 
-void update(mlx_key_data_t keydata, void *param)
+void update(void *param)
 {
 	cub3d_t *cub3d;
 
 	cub3d = param;
-	handle_escape_key(&keydata, cub3d->mlx);
-	get_input(&keydata, cub3d);
 	player_movement(cub3d);
 	// draw_background(cub3d);
 	// raycasting(cub3d);
@@ -67,7 +69,8 @@ void start_game(cub3d_t *cub3d)
 	// raycasting(cub3d);
 	minimap(cub3d);
 	mlx_close_hook(cub3d->mlx, &handle_close_window, cub3d->mlx);
-	mlx_key_hook(cub3d->mlx, &update, cub3d);
+	mlx_key_hook(cub3d->mlx, &get_input, cub3d);
+	mlx_loop_hook(cub3d->mlx, &update, cub3d);
 	mlx_loop(cub3d->mlx);
 	mlx_terminate(cub3d->mlx);
 }

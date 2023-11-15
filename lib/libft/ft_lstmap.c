cad7a86976d55b113824bb82d30d4a363371b059
jbagger <jbagger@student.hive.fi>
2023-11-15 10:46:29 +0200

    Added list functions to libft

---
+++ lib/libft/ft_lstmap.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbagger <jbagger@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/08 18:35:24 by marvin            #+#    #+#             */
+/*   Updated: 2023/02/22 08:43:39 by jbagger          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*temp;
+	t_list	*i;
+
+	if (!lst && !f)
+		return (NULL);
+	temp = NULL;
+	while (lst)
+	{
+		if (!(i = ft_lstnew(f(lst->content))))
+		{
+			ft_lstclear(&temp, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&temp, i);
+		lst = lst->next;
+		i = i->next;
+	}
+	return (temp);
+}

e0cba5f76243fbc4baf045a59fa1eee38c312638
Sakari Lampinen <slampine@c3r5p3.hive.fi>
2023-11-17 14:47:55 +0200

    started working on new raycasting, work in progress

--- src/raycasting/raycasting.c
+++ src/raycasting/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: slampine <slampine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 12:14:17 by slampine          #+#    #+#             */
-/*   Updated: 2023/11/16 14:03:05 by slampine         ###   ########.fr       */
+/*   Updated: 2023/11/17 14:09:38 by slampine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,69 +46,96 @@ static void	set_wall_direction(ray_t *ray, player_t *player, int wall_flag)
 
 //------------------------------------------------------------------------------
 
+// int	find_first_horizontal_wall(cub3d_t *cub3d, player_t *player, ray_t *ray)
+// {
+// 	dvector_t	vRayDir;
+// 	dvector_t	deltaH;
+// 	vector_t	vMapCheckH;
+// 	double		max_dist;
+
+// 	vRayDir.x = cos(ray->angle);
+// 	vRayDir.y = sin(ray->angle);
+// 	if (vRayDir.x == 0)
+// 		return (FAIL);
+// 	max_dist = sqrt(cub3d->img->width * cub3d->img->width + cub3d->img->height * cub3d->img->height);
+// 	vMapCheckH.y = floor(player->pos.y);
+// 	deltaH.y = 1;
+// 	if (vRayDir.y > 0)
+// 	{
+// 		deltaH.y = -1;
+// 		vMapCheckH.y++;
+// 	}
+// 	vMapCheckH.x = player->pos.x + (player->pos.y - vMapCheckH.y) / tan(ray->angle);
+// }
+// link to graph https://virtual-graph-paper.com/NjY5NTkyNWI5ZDE4
 int	raycast(cub3d_t *cub3d, player_t *player, ray_t *ray)
 {
-	dvector_t	vRayUnitStepSize;
-	dvector_t	vRayLength1D;
 	dvector_t	vRayDir;
-	vector_t	vMapCheck;
-	vector_t	vStep;
+	dvector_t	deltaH;
+	dvector_t	deltaV;
+	vector_t	vMapCheckH;
+	vector_t	vMapCheckV;
 	int			end_found;
+	double		max_dist;
+	int			wall_flag;
 
 	end_found = FALSE;
 	vRayDir.x = cos(ray->angle);
 	vRayDir.y = sin(ray->angle);
 	if (vRayDir.x == 0 || vRayDir.y == 0)
 		return (FAIL);
-	vRayUnitStepSize.x = sqrt(1 + (vRayDir.y / vRayDir.x) * (vRayDir.y / vRayDir.x));
-	vRayUnitStepSize.y = sqrt(1 + (vRayDir.x / vRayDir.y) * (vRayDir.x / vRayDir.y));
-	vMapCheck.x = (int)player->pos.x;
-	vMapCheck.y = (int)player->pos.y;
+	max_dist = sqrt(cub3d->img->width * cub3d->img->width + cub3d->img->height * cub3d->img->height);
+	wall_flag = 0;
+	vMapCheckH.y = ceil(player->pos.y);
+	deltaH.y = 1;
+	if (vRayDir.y > 0)
+	{
+		deltaH.y = -1;
+		vMapCheckH.y--;
+	}
+	vMapCheckV.x = ceil(player->pos.x);
+	deltaV.x = 1;
 	if (vRayDir.x < 0)
 	{
-		vStep.x = -1;
-		vRayLength1D.x = (player->pos.x - vMapCheck.x) * vRayUnitStepSize.x;
+		deltaV.x = -1;
+		vMapCheckV.x--;
 	}
-	else
-	{
-		vStep.x = 1;
-		vRayLength1D.x = (vMapCheck.x + 1.0 - player->pos.x) * vRayUnitStepSize.x;
-	}
-	if (vRayDir.y < 0)
-	{
-		vStep.y = -1;
-		vRayLength1D.y = (player->pos.y - vMapCheck.y) * vRayUnitStepSize.y;
-	}
-	else
-	{
-		vStep.y = 1;
-		vRayLength1D.y = (vMapCheck.y + 1.0 - player->pos.y) * vRayUnitStepSize.y;
-	}
-	double max_dist = sqrt(cub3d->img->width * cub3d->img->width + cub3d->img->height * cub3d->img->height);
-	int wall_flag = 0;
+	printf("\nStarting:\nplayer.pos is %f,%f\n",player->pos.x, player->pos.y);
+	printf("vRaydir are %f and %f\n",vRayDir.x,vRayDir.y);
+	printf("player angle is %f, ray angle is %f\n", player->angle / M_PI * 180, ray->angle / M_PI * 180);
+	vMapCheckH.x = player->pos.x + (player->pos.y - vMapCheckH.y) / tan(ray->angle);
+	deltaH.x = 1 / tan(ray->angle);
+	printf("First horizontal hit at pos %i,%i\n", vMapCheckH.x, vMapCheckH.y);
+	printf("deltaHX is %f, deltaHY is %f\n", deltaH.x, deltaH.y);
+	printf("Horizontal done\n--------------------\n");
+	vMapCheckV.y = player->pos.y + (player->pos.x - vMapCheckV.x) / tan(ray->angle);
+	printf("First vertical hit at pos %i,%i\n", vMapCheckV.x, vMapCheckV.y);
+	deltaV.y = tan(ray->angle);
+	printf("deltaVX is %f, deltaVY is %f\n", deltaV.x, deltaV.y);
+	printf("Vertical done\n--------------------");
 	while (!end_found && ray->length < max_dist)
 	{
-		if (vRayLength1D.x < vRayLength1D.y)
+		if (wall_found(cub3d, vMapCheckH))
 		{
-			vMapCheck.x += vStep.x;
-			ray->length = vRayLength1D.x;
-			vRayLength1D.x += vRayUnitStepSize.x;
-			wall_flag = X;
-		}
-		else
-		{
-			vMapCheck.y += vStep.y;
-			ray->length = vRayLength1D.y;
-			vRayLength1D.y += vRayUnitStepSize.y;
-			wall_flag = Y;
-		}
-		if (wall_found(cub3d, vMapCheck))
-		{
-			ray->target = cub3d->map[vMapCheck.y][vMapCheck.x];
+			printf("\n\nFound horizontal wall\n\n");
+			ray->target = cub3d->map[vMapCheckH.y][vMapCheckH.x];
+			wall_flag = 0;
 			update_end(cub3d, &vRayDir, ray, &end_found);
 		}
+		else if (wall_found(cub3d, vMapCheckV))
+		{
+			printf("\n\nFound vertical wall\n\n");
+			ray->target = cub3d->map[vMapCheckV.y][vMapCheckV.x];
+			wall_flag = 1;
+			update_end(cub3d, &vRayDir, ray, &end_found);
+		}
+		vMapCheckH.x += deltaH.x;
+		vMapCheckH.y += deltaH.y;
+		vMapCheckV.x += deltaV.x;
+		vMapCheckV.y += deltaV.y;
 	}
 	set_wall_direction(ray, player, wall_flag);
+	ray->length = 1;
 	return (SUCCESS);
 }
 

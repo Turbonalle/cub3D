85636e0c58b2fa2922ee70d924f9b1c707841a98
jbagger <jbagger@student.hive.fi>
2023-12-18 15:16:21 +0200

    Finishing a speedrun lvl now takes you to lvl menu

--- src/core/game_over.c
+++ src/core/game_over.c
@@ -21,7 +21,6 @@ int	is_top_score(level_t *level, int time)
 void	game_over(cub3d_t *cub3d)
 {
 	cub3d->time_finished = (int)(elapsed_time(cub3d) * 1000);
-	cub3d->speedrun = FALSE;
 	mlx_delete_image(cub3d->mlx, cub3d->minimap.img);
 	free_level(cub3d);
 	if (cub3d->level != &cub3d->levels[0] && is_top_score(cub3d->level, cub3d->time_finished))
@@ -30,11 +29,18 @@ void	game_over(cub3d_t *cub3d)
 		cub3d->state = STATE_ENTERNAME;
 		handle_cursor(cub3d);
 	}
+	else if (cub3d->speedrun)
+	{
+		enable_level_menu(&cub3d->level_menu);
+		cub3d->speedrun = FALSE;
+		cub3d->state = STATE_LEVEL;
+		handle_cursor(cub3d);
+	}
 	else
 	{
 		enable_start_menu(&cub3d->start_menu);
+		cub3d->speedrun = FALSE;
 		cub3d->state = STATE_START;
 		handle_cursor(cub3d);
-		// cub3d->state = STATE_GAMEOVER;
 	}
 }

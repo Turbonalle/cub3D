fcba0ed901d8d75abbc09394859c84bcb483a45e
Valeria Vagapova <lera.vagapova@gmail.com>
2023-12-18 13:41:10 +0200

    avoid showing parts of keys that are hidden behind corners

--- src/animation/keys_animation.c
+++ src/animation/keys_animation.c
@@ -1,28 +1,23 @@
 #include "../incl/cub3d.h"
 
-void scale_curr_frame(mlx_image_t *res, mlx_texture_t *src, double factor, vector_t pos_screen)
+void scale_curr_frame(cub3d_t *cub3d, key_node_t *key, mlx_texture_t *src, double factor)
 {
 	uint32_t	row_src;
 	uint32_t	col_src;
 	uint32_t	row_res;
 	uint32_t	col_res;
 
-	//int		pixel;	
-	//int i;
-
+	ft_memset(key->img_curr_frame->pixels, 0, key->img_curr_frame->width * key->img_curr_frame->height * 4);
 	row_res = 0;
-	//printf("factor: %f\n", factor);
-	//printf("RES height: %f, width: %f\n", src->height * factor, src->width * factor);
-	ft_memset(res->pixels, 0, res->width * res->height * 4);
 	while (row_res < src->height * factor)
 	{
 		col_res = 0;
-		if (row_res < res->height)
+		if (row_res < key->img_curr_frame->height)
 		{
 			while (col_res < src->width * factor)
 			{
 				// TODO: handle out of limits pixels
-				if (col_res < res->width)
+				if (col_res < key->img_curr_frame->width)
 				{
 					row_src = (uint32_t)round(row_res / factor);
 					// make sure that source pixel is not out of limits
@@ -31,9 +26,10 @@ void scale_curr_frame(mlx_image_t *res, mlx_texture_t *src, double factor, vecto
 					col_src = (uint32_t)round(col_res / factor);
 					if (col_src >= src->width)
 						col_src--;
-					ft_memcpy(res->pixels + row_res * res->width * 4 + col_res * 4,
-						src->pixels + row_src * src->width * 4 + col_src * 4,
-						4);
+					if (cub3d->rays[(int)(key->pos_screen.x - src->width * factor * 0.5 + col_res)].length > key->dist_to_player)
+						ft_memcpy(key->img_curr_frame->pixels + row_res * key->img_curr_frame->width * 4 + col_res * 4,
+							src->pixels + row_src * src->width * 4 + col_src * 4,
+							4);
 				}
 				col_res++;
 				// Maybe optimise and skip column completely?
@@ -42,8 +38,8 @@ void scale_curr_frame(mlx_image_t *res, mlx_texture_t *src, double factor, vecto
 		row_res++;
 	}
 	//printf("pos_screen: x: %d, y: %d\n", pos_screen.x, pos_screen.y);
-	res->instances[0].x = pos_screen.x - src->width * factor * 0.5;
-	res->instances[0].y = pos_screen.y - src->height * factor * 1.5;
+	key->img_curr_frame->instances[0].x = key->pos_screen.x - src->width * factor * 0.5;
+	key->img_curr_frame->instances[0].y = key->pos_screen.y - src->height * factor * 1.5;
 	//printf("instance pos: x: %d, y: %d\n", res->instances[0].x, res->instances[0].y);
 }
 
@@ -73,10 +69,10 @@ void	draw_keys(cub3d_t *cub3d, int group_index, int curr_frame_num)
 			tmp->img_curr_frame->instances[0].enabled = TRUE;
 			scale_factor = calculate_scale_factor(tmp->dist_to_player);
 			scale_curr_frame(
-				tmp->img_curr_frame,
+				cub3d,
+				tmp,
 				cub3d->level->key_groups[group_index].textures_frames[curr_frame_num],
-				scale_factor,
-				tmp->pos_screen);
+				scale_factor);
 		}
 		else if (tmp->visible == FALSE || tmp->collected == TRUE)
 		{

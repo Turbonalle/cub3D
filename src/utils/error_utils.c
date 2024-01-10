b6d29f37237fdf638ecdcfba6491fd8757ce80c3
jbagger <jbagger@student.hive.fi>
2024-01-10 14:14:17 +0200

    Fixed map textures

--- src/utils/error_utils.c
+++ src/utils/error_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vvagapov <vvagapov@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: jbagger <jbagger@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 15:52:22 by vvagapov          #+#    #+#             */
-/*   Updated: 2023/12/06 18:46:51 by vvagapov         ###   ########.fr       */
+/*   Updated: 2024/01/10 14:09:28 by jbagger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	err(char *error_message)
 {
 	// TODO: potentially set game state to CRASH
 	printf(TERMINAL_RED);
-	printf("Error: %s\n", error_message);
+	printf("Error:\n%s\n", error_message);
 	printf(TERMINAL_RESET);
 	return (FAIL);
 }

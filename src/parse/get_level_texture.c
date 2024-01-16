356f3d1261e444e5e13e0d009ea509b925310419
Valeria Vagapova <lera.vagapova@gmail.com>
2024-01-16 14:12:12 +0200

    norm pause and parse

--- src/parse/get_level_texture.c
+++ src/parse/get_level_texture.c
@@ -6,7 +6,7 @@
 /*   By: vvagapov <vvagapov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 09:09:17 by slampine          #+#    #+#             */
-/*   Updated: 2024/01/14 20:16:38 by vvagapov         ###   ########.fr       */
+/*   Updated: 2024/01/16 14:11:59 by vvagapov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int	get_texture(level_t *level, int element, char **info)
 	free_info(info);
 	if (!level->texture[element].path)
 		return (err("Failed to allocate memory"));
-	level->texture[element].texture = mlx_load_png(level->texture[element].path);
-	//printf("Texture[%d] pointer get_texture: %p\n", element, level->texture[element].texture);
+	level->texture[element].texture
+		= mlx_load_png(level->texture[element].path);
 	if (!level->texture[element].texture)
 		return (err("Failed to load texture"));
 	return (SUCCESS);
--- src/pause/pause_text.c
+++ src/pause/pause_text.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pause_text.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: slampine <slampine@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: vvagapov <vvagapov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 15:08:13 by slampine          #+#    #+#             */
-/*   Updated: 2024/01/11 15:13:40 by slampine         ###   ########.fr       */
+/*   Updated: 2024/01/16 14:11:25 by vvagapov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,62 @@
 
 void	add_category_text(cub3d_t *cub3d, pause_menu_t *menu)
 {
-	menu->text_fps = mlx_put_string(cub3d->mlx, "FPS:", menu->pos_col_text, menu->pos_row_1);
-	menu->text_fps->instances[0].y = menu->pos_row_1 + menu->box_fps[0].size * 0.5 - menu->text_fps->height * 0.5;
-	menu->text_fisheye = mlx_put_string(cub3d->mlx, "Fisheye:", menu->pos_col_text, menu->pos_row_2);
-	menu->text_fisheye->instances[0].y = menu->pos_row_2 + menu->box_fps[0].size * 0.5 - menu->text_fisheye->height * 0.5;
-	menu->text_mouse = mlx_put_string(cub3d->mlx, "Mouse:", menu->pos_col_text, menu->pos_row_3);
-	menu->text_mouse->instances[0].y = menu->pos_row_3 + menu->box_fps[0].size * 0.5 - menu->text_mouse->height * 0.5;
-	menu->text_mouse_sensitivity = mlx_put_string(cub3d->mlx, "Sensitivity:", menu->pos_col_text, menu->pos_row_4);
-	menu->text_mouse_sensitivity->instances[0].y = menu->pos_row_4 + menu->box_fps[0].size * 0.5 - menu->text_mouse_sensitivity->height * 0.5;
+	menu->text_fps = mlx_put_string(cub3d->mlx, "FPS:", menu->pos_col_text,
+			menu->pos_row_1);
+	menu->text_fps->instances[0].y = menu->pos_row_1 + menu->box_fps[0].size
+		* 0.5 - menu->text_fps->height * 0.5;
+	menu->text_fisheye = mlx_put_string(cub3d->mlx, "Fisheye:",
+			menu->pos_col_text, menu->pos_row_2);
+	menu->text_fisheye->instances[0].y = menu->pos_row_2 + menu->box_fps[0]
+		.size * 0.5 - menu->text_fisheye->height * 0.5;
+	menu->text_mouse = mlx_put_string(cub3d->mlx, "Mouse:", menu->pos_col_text,
+			menu->pos_row_3);
+	menu->text_mouse->instances[0].y = menu->pos_row_3 + menu->box_fps[0].size
+		* 0.5 - menu->text_mouse->height * 0.5;
+	menu->text_mouse_sensitivity = mlx_put_string(cub3d->mlx, "Sensitivity:",
+			menu->pos_col_text, menu->pos_row_4);
+	menu->text_mouse_sensitivity->instances[0].y = menu->pos_row_4
+		+ menu->box_fps[0].size * 0.5
+		- menu->text_mouse_sensitivity->height * 0.5;
+}
+
+void	add_fps_checkbox_text(cub3d_t *cub3d, pause_menu_t *menu)
+{
+	menu->box_fps[0].text = mlx_put_string(cub3d->mlx, "15",
+			menu->pos_col_box_1 + menu->box_fps[0].size * 0.5,
+			menu->pos_row_1 + menu->box_fps[0].size * 0.5);
+	center(menu->box_fps[0].text);
+	menu->box_fps[1].text = mlx_put_string(cub3d->mlx, "30",
+			menu->pos_col_box_2 + menu->box_fps[1].size * 0.5,
+			menu->pos_row_1 + menu->box_fps[1].size * 0.5);
+	center(menu->box_fps[1].text);
+	menu->box_fps[2].text = mlx_put_string(cub3d->mlx, "60",
+			menu->pos_col_box_3 + menu->box_fps[2].size * 0.5,
+			menu->pos_row_1 + menu->box_fps[2].size * 0.5);
+	center(menu->box_fps[2].text);
+	menu->box_fps[3].text = mlx_put_string(cub3d->mlx, "120",
+			menu->pos_col_box_4 + menu->box_fps[3].size * 0.5,
+			menu->pos_row_1 + menu->box_fps[3].size * 0.5);
+	center(menu->box_fps[3].text);
 }
 
 void	add_checkbox_text(cub3d_t *cub3d, pause_menu_t *menu)
 {
-	menu->box_fps[0].text = mlx_put_string(cub3d->mlx, "15", menu->pos_col_box_1 + menu->box_fps[0].size * 0.5, menu->pos_row_1 + menu->box_fps[0].size * 0.5);
-	center(menu->box_fps[0].text);
-	menu->box_fps[1].text = mlx_put_string(cub3d->mlx, "30", menu->pos_col_box_2 + menu->box_fps[1].size * 0.5, menu->pos_row_1 + menu->box_fps[1].size * 0.5);
-	center(menu->box_fps[1].text);
-	menu->box_fps[2].text = mlx_put_string(cub3d->mlx, "60", menu->pos_col_box_3 + menu->box_fps[2].size * 0.5, menu->pos_row_1 + menu->box_fps[2].size * 0.5);
-	center(menu->box_fps[2].text);
-	menu->box_fps[3].text = mlx_put_string(cub3d->mlx, "120", menu->pos_col_box_4 + menu->box_fps[3].size * 0.5, menu->pos_row_1 + menu->box_fps[3].size * 0.5);
-	center(menu->box_fps[3].text);
-	menu->box_fisheye[0].text = mlx_put_string(cub3d->mlx, "On", menu->pos_col_box_1 + menu->box_fisheye[0].size * 0.5, menu->pos_row_2 + menu->box_fisheye[0].size * 0.5);
+	add_fps_checkbox_text(cub3d, menu);
+	menu->box_fisheye[0].text = mlx_put_string(cub3d->mlx, "On",
+			menu->pos_col_box_1 + menu->box_fisheye[0].size * 0.5,
+			menu->pos_row_2 + menu->box_fisheye[0].size * 0.5);
 	center(menu->box_fisheye[0].text);
-	menu->box_fisheye[1].text = mlx_put_string(cub3d->mlx, "Off", menu->pos_col_box_2 + menu->box_fisheye[1].size * 0.5, menu->pos_row_2 + menu->box_fisheye[1].size * 0.5);
+	menu->box_fisheye[1].text = mlx_put_string(cub3d->mlx, "Off",
+			menu->pos_col_box_2 + menu->box_fisheye[1].size * 0.5,
+			menu->pos_row_2 + menu->box_fisheye[1].size * 0.5);
 	center(menu->box_fisheye[1].text);
-	menu->box_mouse[0].text = mlx_put_string(cub3d->mlx, "On", menu->pos_col_box_1 + menu->box_mouse[0].size * 0.5, menu->pos_row_3 + menu->box_mouse[0].size * 0.5);
+	menu->box_mouse[0].text = mlx_put_string(cub3d->mlx, "On",
+			menu->pos_col_box_1 + menu->box_mouse[0].size * 0.5,
+			menu->pos_row_3 + menu->box_mouse[0].size * 0.5);
 	center(menu->box_mouse[0].text);
-	menu->box_mouse[1].text = mlx_put_string(cub3d->mlx, "Off", menu->pos_col_box_2 + menu->box_mouse[1].size * 0.5, menu->pos_row_3 + menu->box_mouse[1].size * 0.5);
+	menu->box_mouse[1].text = mlx_put_string(cub3d->mlx, "Off",
+			menu->pos_col_box_2 + menu->box_mouse[1].size * 0.5,
+			menu->pos_row_3 + menu->box_mouse[1].size * 0.5);
 	center(menu->box_mouse[1].text);
 }
